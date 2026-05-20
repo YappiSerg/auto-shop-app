512c89304c62bb94e81fece32120f435728a87e5
Egor Diac <egordiac@yandex.ru>
2026-05-20 21:16:36 +0300

    ugh 3

--- setup.py
+++ setup.py
@@ -1,27 +1,18 @@
 from setuptools import setup
-
-try:
-    from sphinx.setup_command import BuildDoc
-except ImportError:
-    BuildDoc = None
-
-
-cmdclass = {}
-command_options = {}
-
-if BuildDoc is not None:
-    cmdclass['build_sphinx'] = BuildDoc
-    command_options['build_sphinx'] = {
-        'project': ('setup.py', 'Auto Shop App'),
-        'version': ('setup.py', '0.1.0'),
-        'release': ('setup.py', '0.1.0'),
-        'source_dir': ('setup.py', 'docs'),
-        'build_dir': ('setup.py', 'docs/_build'),
-        'builder': ('setup.py', 'html'),
-    }
+from sphinx.setup_command import BuildDoc
 
 
 setup(
-    cmdclass=cmdclass,
-    command_options=command_options,
+    cmdclass={'build_sphinx': BuildDoc},
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', 'Auto Shop App'),
+            'version': ('setup.py', '0.1.0'),
+            'release': ('setup.py', '0.1.0'),
+            'source_dir': ('setup.py', 'docs'),
+            'build_dir': ('setup.py', 'docs/_build'),
+            'builder': ('setup.py', 'html'),
+        }
+    },
+    setup_requires=['Sphinx>=7.0,<9.0'],
 )
